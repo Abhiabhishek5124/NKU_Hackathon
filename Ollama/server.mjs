2cac3335ac2adfc3e548f15c3cc4b0d2e033371e
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-04-19 16:18:04 -0400

    Model Trained

--- Ollama/server.mjs
+++ Ollama/server.mjs
@@ -2,21 +2,45 @@ import ollama from 'ollama'
 import express from 'express';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import * as mammoth from 'mammoth';
 const app = express();
 const port = 3009;
 app.use(express.static('./pages'));
 app.use(bodyParser.json());
 //read the file
-//const fileContents = fs.readFileSync('./assets/testAbstract.docx', 'utf-8');
+const docxFilePath = './assets/Income.docx';
+let fileContent = '';
+fs.readFile(docxFilePath, (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+  // Convert .docx to plain text
+  mammoth.extractRawText({ buffer: data })
+    .then(result => {
+      fileContent = result.value;
+      console.log(fileContent);// Print the content to the console
+    })
+    .catch(error => {
+      console.error('Error extracting text from .docx:', error);
+    });
+});
+let modelContent = `You are an expert in creating practice questions based on study material.
+Your goal is to prepare a student about the financial literacy. You do this by asking questions from the source below.:
+
+------------
+${fileContent}
+------------
+
+Make sure the questions are straight `
 //console.log(fileContents);
 
 //generate the question
-
-let question = "When was GitHub first created"
 const responseOllama = await ollama.chat({
   model: 'mistral',
-  messages: [{ role: 'user', content: question }],
+  messages: [{ role: 'user', content: modelContent }],
 })
+
 app.get('/', (req, res) => {
   const myPage = fs.readFileSync('./testFile.html', 'utf8');
   res.setHeader('Content-Type', 'text/html'); 
@@ -24,10 +48,9 @@ app.get('/', (req, res) => {
 });
 app.post('/askquestion', (req, res) => {
   const responseObject = {
-    question: question,
-    answer: responseOllama.message.content
+    question: responseOllama.message.content
   };
-  console.log(responseObject.answer);
+  //console.log(fileContent);
   res.send(responseObject);
 });
 
