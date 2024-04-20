6a1f8f404b07062e519037be9bdf50c8ebfb5509
Bhuwan Bhandari <bhuwanbhandari2821@gmail.com>
2024-04-19 20:11:39 -0400

    Quiz page

--- Ollama/server.mjs
+++ Ollama/server.mjs
@@ -5,7 +5,7 @@ import fs from 'fs';
 import * as mammoth from 'mammoth';
 const app = express();
 const port = 3009;
-app.use(express.static('./pages'));
+app.use(express.static('/Users/bhuwanbhandari/Desktop/NKU_HACKATHON'));
 app.use(bodyParser.json());
 //read the file
 const docxFilePath = './assets/Income.docx';
@@ -19,32 +19,32 @@ fs.readFile(docxFilePath, (err, data) => {
   mammoth.extractRawText({ buffer: data })
     .then(result => {
       fileContent = result.value;
-      console.log(fileContent);// Print the content to the console
+      //console.log(fileContent);// Print the content to the console
     })
     .catch(error => {
       console.error('Error extracting text from .docx:', error);
     });
 });
-let modelContent = `You are an expert in creating practice questions based on study material.
-Your goal is to prepare a student about the financial literacy. You do this by asking questions from the source below.:
 
-------------
-${fileContent}
-------------
-
-Make sure the questions are straight `
 //console.log(fileContents);
 
 //generate the question
 const responseOllama = await ollama.chat({
   model: 'mistral',
-  messages: [{ role: 'user', content: modelContent }],
+  messages: [{ role: 'user', content:`Create ten questions related to financial literacy and create a json array with the question, four options and answer. Just give the 
+  JSON array and nothing else` }],
 })
+let answer = responseOllama.message.content;
+let jsonAnswer = JSON.parse(answer);
 
 app.get('/', (req, res) => {
-  const myPage = fs.readFileSync('./testFile.html', 'utf8');
+  const myPage = fs.readFileSync('./debtQuiz.html', 'utf8');
   res.setHeader('Content-Type', 'text/html'); 
   res.send(myPage);
+});
+app.get('/data.json', (req, res) => {
+  res.send(jsonAnswer);
+  console.log("Json file sent")
 });
 app.post('/askquestion', (req, res) => {
   const responseObject = {
